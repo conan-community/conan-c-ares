7cdaba848e249edec68ea88a83cab80bc6b301c4
Uilian Ries <uilianries@gmail.com>
2018-04-13 19:07:46 -0300

    Add CMake wrapper
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,6 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from conans import ConanFile, CMake, tools
 
 
@@ -6,12 +8,15 @@ class CAresConan(ConanFile):
     name = "c-ares"
     version = "1.14.0"
     license = "MIT"
-    url = "<Package recipe repository url here, for issues about the package>"
+    url = "https://github.com/conan-community/conan-c-ares"
     description = "A C library for asynchronous DNS requests"
-    website = "https://c-ares.haxx.se/"
+    author = "lasote"
+    homepage = "https://c-ares.haxx.se/"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
+    exports = "LICENSE"
+    exports_sources = "CMakeLists.txt"
     generators = "cmake"
 
     def config_options(self):
@@ -21,43 +26,33 @@ class CAresConan(ConanFile):
     def configure(self):
         del self.settings.compiler.libcxx
 
-    @property
-    def folder_name(self):
-        return "c-ares-cares-%s" % self.version.replace(".", "_")
-
     def source(self):
         ver = self.version.replace(".", "_")
         tools.get("https://github.com/c-ares/c-ares/archive/cares-%s.tar.gz" % ver)
 
-    def _patch_cmake(self):
-        conan_magic_lines = '''
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-CONAN_BASIC_SETUP()
-'''
-        tools.replace_in_file(os.path.join(self.source_folder, self.folder_name, "CMakeLists.txt"),
-                              "PROJECT (c-ares C)",
-                              "PROJECT (c-ares C)\n%s" % conan_magic_lines)
-
-    def build(self):
-        self._patch_cmake()
+    def configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["CARES_STATIC"] = "ON" if not self.options.shared else "OFF"
-        cmake.definitions["CARES_SHARED"] = "ON" if self.options.shared else "OFF"
+        cmake.definitions["CARES_STATIC"] = not self.options.shared
+        cmake.definitions["CARES_SHARED"] = self.options.shared
         cmake.definitions["CARES_BUILD_TESTS"] = "OFF"
         cmake.definitions["CARES_MSVC_STATIC_RUNTIME"] = "OFF"  # Do not mess with runtime, Conan will.
         if self.settings.os != "Windows":
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
-        cmake.configure(source_folder=self.folder_name)
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        self.copy("*LICENSE*", dst="", keep_path=False)
+        self.copy("LICENSE.md", dst="licenses", keep_path=False)
+        cmake = self.configure_cmake()
+        cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs.append("cares")
+        self.cpp_info.libs = tools.collect_libs(self)
         if not self.options.shared:
             self.cpp_info.defines.append("CARES_STATICLIB")
         if self.settings.os == "Windows":
-            # self.cpp_info.libs[0] += "d"
             self.cpp_info.libs.append("ws2_32")
