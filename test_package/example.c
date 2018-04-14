09c1a46f082836e0f7961cfcf895e0b20fb63ca9
Uilian Ries <uilianries@gmail.com>
2018-04-13 23:02:51 -0300

    Build on mingw
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/example.c
+++ test_package/example.c
@@ -16,6 +16,21 @@
     #include <winsock2.h>
 #endif
 
+#ifdef __MINGW32__
+    const char* inet_ntop(int af, const void* src, char* dst, int cnt) {
+        struct sockaddr_in srcaddr;
+        memset(&srcaddr, 0, sizeof(struct sockaddr_in));
+        memcpy(&(srcaddr.sin_addr), src, sizeof(srcaddr.sin_addr));
+
+        srcaddr.sin_family = af;
+        if (WSAAddressToString((struct sockaddr*) &srcaddr, sizeof(struct sockaddr_in), 0, dst, (LPDWORD) &cnt) != 0) {
+            DWORD rv = WSAGetLastError();
+            return NULL;
+        }
+        return dst;
+    }
+#endif // __MINGW32__
+
 static void
 state_cb(void *data, int s, int read, int write)
 {
